92a954d57db70e334e27b8c123fec9d578a62db3
Jan Eckert <jan.eckert@tum.de>
2023-06-19 19:49:36 +0200

    Ran prettier

--- src/api/chargers.ts
+++ src/api/chargers.ts
@@ -1,80 +1,90 @@
-import chargingStations from 'assets/data/chargingStationsBNE.json'
+import chargingStations from 'assets/data/chargingStationsBNE.json';
 import Supercluster from 'supercluster';
 
 // Typing
 type ChargingStation = {
-    operator: string;
-    location: ChargingStationLocation;
-    commissioningDate: string;
-    totalCapacity: string;
-    type: string;
-    chargerCount: number;
-    chargerInfo?: (ChargingStationChargerInfo)[] | null;
-}
+  operator: string;
+  location: ChargingStationLocation;
+  commissioningDate: string;
+  totalCapacity: string;
+  type: string;
+  chargerCount: number;
+  chargerInfo: ChargingStationChargerInfo[];
+};
 type ChargingStationLocation = {
-    street: string;
-    number: string;
-    supplement: string;
-    city: string;
-    district: string;
-    state: string;
-    zipCode: string;
-    lat: number;
-    lng: number;
-}
+  street: string;
+  number: string;
+  supplement: string;
+  city: string;
+  district: string;
+  state: string;
+  zipCode: string;
+  lat: number;
+  lng: number;
+};
+
+type ChargingStationConnectorType =
+  | 'Type2'
+  | 'CCS'
+  | 'CHAdeMO'
+  | 'Schuko'
+  | 'CEE'
+  | 'CEE3Pin'
+  | 'CEE5Pin'
+  | 'Tesla'
+  | 'Type2Tesla';
+
 type ChargingStationChargerInfo = {
-    capacity?: number | null;
-    connectorTypes?: (string | null)[] | null;
-    publicKey: string;
-}
+  capacity: number;
+  connectorTypes: ChargingStationConnectorType[];
+  publicKey: string;
+};
 
 // If we include cluster here, we can use it as a discriminated union
 type ChargingStationGeoJSONProperties = {
-    cluster: false,
-    chargingStation: ChargingStation
-}
+  cluster: false;
+  chargingStation: ChargingStation;
+};
 
-export type ChargingStationGeoJSONFeatures = (
-    Supercluster.ClusterFeature<{}> | 
-    Supercluster.PointFeature<ChargingStationGeoJSONProperties>
-)
-
+export type ChargingStationGeoJSONFeatures =
+  | Supercluster.ClusterFeature<{}>
+  | Supercluster.PointFeature<ChargingStationGeoJSONProperties>;
 
 // Static Supercluster instance
 const clusterIndex = new Supercluster<ChargingStationGeoJSONProperties, {}>({
-    radius: 50,
-    maxZoom: 16
-})
-clusterIndex.load((chargingStations as ChargingStation[]).map(cs => ({
+  radius: 50,
+  maxZoom: 16,
+});
+clusterIndex.load(
+  (chargingStations as ChargingStation[]).map((cs) => ({
     type: 'Feature',
     geometry: {
-        type: "Point",
-        coordinates: [cs.location.lat, cs.location.lng]
+      type: 'Point',
+      coordinates: [cs.location.lat, cs.location.lng],
     },
     properties: {
-        cluster: false,
-        chargingStation: cs
-    }
-})))
-
-
+      cluster: false,
+      chargingStation: cs,
+    },
+  })),
+);
 
 export const getClusteredChargingStations = async (
-    bbox: [number, number, number, number],
-    zoom: number
+  bbox: [number, number, number, number],
+  zoom: number,
 ): Promise<ChargingStationGeoJSONFeatures[]> => {
-    const clusters = clusterIndex.getClusters(bbox, zoom)
-    clusters.sort((a, b) => {
-        // If both are clusters, sort by size
-        if(a.properties.cluster && b.properties.cluster) {
-            return a.properties.point_count - b.properties.point_count
-        }
-        // Prefer stations over clusters
-        if(a.properties.cluster) return -1;
-        if(b.properties.cluster) return  1;
+  const clusters = clusterIndex.getClusters(bbox, zoom);
+  clusters.sort((a, b) => {
+    // If both are clusters, sort by size
+    if (a.properties.cluster && b.properties.cluster) {
+      return a.properties.point_count - b.properties.point_count;
+    }
+    // Prefer stations over clusters
+    if (a.properties.cluster) return -1;
+    if (b.properties.cluster) return 1;
 
-        // stations are considered equal
-        return 0
-    })
-    return await Promise.resolve(clusters)
-}
+    // stations are considered equal
+    return 0;
+  });
+  return await Promise.resolve(clusters);
+};

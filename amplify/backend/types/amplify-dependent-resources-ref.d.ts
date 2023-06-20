507fa81587ecde446bb7dec1178cd7f2893edec8
Klajdi <kbudlla15@epoka.edu.al>
2023-06-20 13:56:50 +0200

    Updates

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "v2gconnectapp26404018": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
